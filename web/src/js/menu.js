0f6ad584eb0433fedca5541be2410b2d95c41847
BuildTools <controld3785@gmail.com>
2022-02-03 19:00:51 +0900

    메뉴 텍스트 overflow 문제 어느 정도 해결

---
+++ web/src/js/menu.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    function fixMenu() {
+        var menu_items = document.querySelectorAll(".menu > *, .menu > * > *")
+        if (menu_items != null) {
+            menu_items.forEach((item, index) => {
+                if (item.style.display == "none" || item.style.display == undefined) {
+                    item.style.display = "inline-block"
+                    var isOverflowing = item.clientWidth < item.scrollWidth || item.clientHeight < item.scrollHeight
+                    item.style.display = "none"
+                } else {
+                    var isOverflowing = item.clientWidth < item.scrollWidth || item.clientHeight < item.scrollHeight
+                }
+                console.log(isOverflowing)
+                if (isOverflowing) {
+                    console.log(item.style.fontSize)
+                    item.style.fontSize = itemitem.style.fontSize
+                }
+            })
+        }
+    }
+
+    fixMenu()
+
+    window.addEventListener("resize", (event) => {
+        fixMenu()
+    })
+})
