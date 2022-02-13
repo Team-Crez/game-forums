55fa9b8993862e977650b7cc7358a84440f524a3
copecone <controld3785@gmail.com>
2022-02-13 16:56:05 +0900

    앱 테스트 4 재수정

--- web/src/js/offline.js
+++ web/src/js/offline.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener("load", () => {
-        navigator.serviceWorker.register("src/js/service_worker.js")
+        navigator.serviceWorker.register("service_worker.js")
     })
 }
